a51124b60c785ff018fcb496c4a8c28f91cd765b
Blake Nicholson <blake@seelio.com>
2015-05-12 13:47:40 -0400

    Updated example code so it works after recent changes.
    * .gitignore
    
      Added test.avro.
    
    * example_reader.js,
      example_writer.js
    
      Updated based upon Writer now inheriting from Transform.

--- example_writer.js
+++ example_writer.js
@@ -3,17 +3,43 @@ var schema = {
             "name": "data",
             "type": "record",
             "fields": [
-                {"name":"key","type": "string"},
-                {"name":"value","type": "string"},
-                {"name":"flag","type": "boolean"},
-				{"name":"subrecord","type":"record","fields":[
-					{"name":"key","type":"string"},
-					{"name":"value","type":["string","int","null"]}
-				]}
+                {"name":"key", "type": "string"},
+                {"name":"value", "type": "string"},
+                {"name":"flag", "type": "boolean"},
+                {"name":"subrecord",
+                 "type":"record",
+                 "fields":[
+                    {"name":"key", "type":"string"},
+                    {"name":"value", "type":["string","int","null"]}
+                ]}
             ]
 };
 var writer = avro.open("test.avro", schema, { flags: 'w', codec: 'deflate' });
-writer
-    .append({ key:"john", value:"hive", flag: true, subrecord: { key: "preference", value: {"int":2}}})
-    .append({ key:"eric", value:"lola", flag: true, subrecord: { key: "postcode", value: {"null":null}}})
-    .end({ key:"fred", value:"wonka", flag: false, subrecord: { key: "city", value: {"string":"michigan"}}});
+writer.write({
+    key: "john",
+    value: "hive",
+    flag: true,
+    subrecord: {
+        key: "preference",
+        value: 2
+    }
+});
+writer.write({
+    key: "eric",
+    value: "lola",
+    flag: true,
+    subrecord: {
+        key: "postcode",
+        value: null
+    }
+});
+writer.write({
+    key: "fred",
+    value: "wonka",
+    flag: false,
+    subrecord: {
+        key: "city",
+        value: "Ann Arbor"
+    }
+});
+writer.end();
